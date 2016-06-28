0bcf58db435ff9f61ddb354c09f64f5d9fe3a0c2
vierello <vierello@hotmail.com>
2016-06-28 14:26:44 -0400

    added dist folders

--- d/app/scripts/router.js
+++ d/app/scripts/router.js
@@ -1,10 +1,16 @@
 var Backbone = require('backbone');
 var $ = require('jquery');
+var models = require('./models/models');
+var views = require('./views/views');
 
 var Router = Backbone.Router.extend({
   routes: {
 
   },
+
+  initialize: function(){
+    
+  }
 })
 
 var router = new Router();
